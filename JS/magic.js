7b77aa3182105fa2bfc478151b0dae6909f248cf
Cameron64 <cameron64@ymail.com>
2015-01-18 17:58:17 -0600

    implemented localstorage

--- JS/magic.js
+++ JS/magic.js
@@ -3,27 +3,7 @@
  */
 (function () {
 
-    var app = angular.module('stream', []);
-
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        var expires = "expires="+d.toUTCString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    }
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0; i<ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1);
-            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-        }
-        return "";
-    }
-
-
+    var app = angular.module('stream', ['ngStorage']);
 
     app.directive('mainbanner', function () {
         return{
@@ -57,75 +37,14 @@
         }
     });
 
-    app.factory('sharedService', function () {
-        var shared = {};
-        shared.isChatHidden = false;
-        shared.isStreamHidden = false;
+    app.controller('StreamSubController', ['$scope', '$localStorage', function ($scope, $localStorage) {
 
-        return shared;
-    });
-
-
-    app.controller('MainController', ['$scope', 'sharedService', function ($scope, sharedService) {
-        $scope.hideChat = false;
-
-        $scope.sharedService = sharedService;
-
-
-        $scope.change = function () {
-
-            //set hide opposite
-            $scope.hideChat = !$scope.hideChat;
-
-            //share to service
-            sharedService.isChatHidden = $scope.hideChat;
-        }
+        $scope.$storage = $localStorage.$default({
+            chat: 0,
+            stream: 0,
+            banner: 0
+        });
 
     }]);
 
-    app.controller('SubController', ['$scope', 'sharedService', function ($scope, sharedService) {
-
-
-        $scope.check = function () {
-            $scope.sharedService = sharedService;
-            $scope.hideChat = $scope.sharedService.isChatHidden;
-            return($scope.hideChat);
-        };
-
-    }]);
-
-    app.controller('StreamController', ['$scope', 'sharedService', function ($scope, sharedService) {
-        $scope.hideStream = false;
-
-        $scope.sharedService = sharedService;
-
-
-        $scope.change = function () {
-
-            //set hide opposite
-            $scope.hideStream = !$scope.hideStream;
-
-            //share to service
-            sharedService.isStreamHidden = $scope.hideStream;
-        }
-
-    }]);
-
-    app.controller('StreamSubController', ['$scope', 'sharedService', function ($scope, sharedService) {
-
-        $scope.check = function() {
-            $scope.sharedService = sharedService;
-            $scope.hideStream = $scope.sharedService.isStreamHidden;
-            return($scope.hideStream);
-        };
-
-
-    }]);
-
-    if(getCookie("bearocracy")){
-        $scope.hideBanner = true;
-    }else{
-        console.log(this.hideBanner);
-        console.log("made it");
-    }
 })();
