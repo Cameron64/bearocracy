a88bee9484221439740b8693995445c5c417370a
Cameron64 <cameron64@ymail.com>
2015-01-26 13:58:52 -0600

    Removed thumbnail preview
    Caused breakages, removing it fixes breakages

--- JS/magic.js
+++ JS/magic.js
@@ -29,7 +29,7 @@
     app.directive('mainToggles', function () {
         return{
             restrict: 'E',
-            templateUrl: 'mainToggles.html',
+            templateUrl: 'maintoggles.html',
             controller: 'MainController'
         }
     });
