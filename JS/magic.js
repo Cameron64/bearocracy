e6f330c1605da817f72160ee3724e9d4b8877678
cameron64 <Cameron64@ymail.com>
2015-06-02 15:41:43 -0500

    minification

--- JS/magic.js
+++ JS/magic.js
@@ -1,53 +1,53 @@
 (function () {
 
-    var app = angular.module('stream', ['ngStorage']);
+    angular.module('stream', ['ngStorage']).
 
-    app.directive('mainbanner', ['$window', function ($window) {
+    directive('mainbanner',  function () {
         return{
             restrict: 'E',
             templateUrl: 'Directives/slideshow.html',
             controller: 'banner'
         };
-    }]);
+    }).
 
-    app.directive('mainToggles', function () {
+    directive('mainToggles', function () {
         return{
             restrict: 'E',
             templateUrl: 'Directives/mainToggles.html',
             controller: 'main'
         }
-    });
+    }).
 
-    app.directive('subToggles', function () {
+    directive('subToggles', function () {
         return{
             restrict: 'E',
             templateUrl: 'Directives/subToggles.html',
             controller: 'main'
         }
-    });
+    }).
 
-    app.directive('volume', function(){
+    directive('volume', function(){
         return{
             restrict: 'E',
             templateUrl: 'Directives/volume.html',
             controller: 'volume'
         }
-    });
+    }).
 
-    app.directive('mainThumbs', function(){
+    directive('mainThumbs', function(){
         return{
             restrict: 'E',
             templateUrl: 'Directives/mainThumbs.html',
             controller: 'main'
         }
-    });
+    }).
 
-    app.directive('subThumbs', function(){
+    directive('subThumbs', function(){
         return{
             restrict: 'E',
             templateUrl: 'Directives/subThumbs.html',
             controller: 'main'
         }
-    });
+    })
 
 })();
